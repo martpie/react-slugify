f33a822378356e32022d7f2d4fdb97c3308d1357
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2021-09-16 19:01:48 +0200

    2.0.4

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
-=======
-  transform: {
-    '^.+\\.(t|j)sx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        jsx: 'react',
-      },
-    },
-  },
-};
->>>>>>> b05425f (Remove multiple following delimiters)
